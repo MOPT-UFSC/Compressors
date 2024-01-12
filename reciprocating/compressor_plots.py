b29a2caa200baa1af2bc7c04dd41cdfbc7e25b06
JacsonGilVargas <jgvargasemc@gmail.com>
2024-01-12 13:12:34 -0300

    First commit

---
+++ reciprocating/compressor_plots.py
@@ -0,0 +1,87 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def plot(x, y, x_label, y_label, title, label="", _absolute=False):
+
+    plt.ion()
+
+    fig = plt.figure(figsize=[8,6])
+    ax_ = fig.add_subplot(1,1,1)
+
+    if _absolute:
+        y = np.abs(y)
+
+    ax_.plot(x, y, color=[1,0,0], linewidth = 2, label = label)
+    ax_.set_xlabel(x_label, fontsize = 11, fontweight = 'bold')
+    ax_.set_ylabel(y_label, fontsize = 11, fontweight = 'bold')
+    ax_.set_title(title, fontsize = 12, fontweight = 'bold')
+    plt.grid()
+    plt.show() 
+
+def plot2(x, y, x_label, y_label, title, labels, colors, linestyles):
+
+    plt.ion()
+
+    fig = plt.figure(figsize=[8,6])
+    ax_ = fig.add_subplot(1,1,1)
+
+    for i, label in enumerate(labels): 
+        ax_.plot(x[i], y[i], color=colors[i], linewidth=2, linestyle=linestyles[i], label=label)
+    
+    ax_.set_xlabel(x_label, fontsize = 11, fontweight = 'bold')
+    ax_.set_ylabel(y_label, fontsize = 11, fontweight = 'bold')
+    ax_.set_title(title, fontsize = 12, fontweight = 'bold')
+    plt.legend()
+    plt.grid()
+    plt.show() 
+
+def plot_2_yaxis(data_to_plot, title):
+
+    plt.ion()
+    fig = plt.figure(figsize=[8,6])
+    ax_1 = fig.add_subplot(1,1,1)
+    ax_2 = ax_1.twinx()
+    
+    if len(data_to_plot) == 2:
+        for key, data in data_to_plot.items():
+
+            if "axis" in data.keys():
+                axis_ = data["axis"]
+            if "x_data" in data.keys():
+                x_data = data["x_data"]
+            if"y_data" in data.keys():
+                y_data = data["y_data"]
+                if data["y_axis_absolute"]:
+                    y_data = np.abs(y_data)
+            if "x_label" in data.keys():
+                x_label = data["x_label"]
+            if "y_label" in data.keys():
+                y_label = data["y_label"]
+            if "legend_label" in data.keys():
+                legend_label = data["legend_label"]
+            if "color" in data.keys():
+                color = data["color"]
+            if "linewidth" in data.keys():
+                linewidth = data["linewidth"]
+            if "linestyle" in data.keys():
+                linestyle = data["linestyle"]
+
+            ax_1.set_xlabel(x_label, fontsize = 11, fontweight = 'bold')
+            plots = []
+            legends = []
+            if axis_ == "left":
+                plot_1, = ax_1.plot(x_data, y_data, color=color, linewidth=linewidth, linestyle=linestyle, label=legend_label)
+                ax_1.set_ylabel(y_label, fontsize = 11, fontweight = 'bold')
+                plots.append(plot_1)
+                legends.append(legend_label)
+            else:
+                plot_2, = ax_2.plot(x_data, y_data, color=color, linewidth=linewidth, linestyle=linestyle, label=legend_label)
+                ax_2.set_ylabel(y_label, fontsize = 11, fontweight = 'bold')
+                plots.append(plot_2)
+                legends.append(legend_label)
+
+        ax_1.set_title(title, fontsize = 12, fontweight = 'bold')
+        ax_1.grid()
+        ax_2.grid()
+        fig.legend(bbox_to_anchor=(1,1), bbox_transform=ax_1.transAxes)
+        plt.show() 
---
+++ reciprocating_compressor.py
@@ -0,0 +1,48 @@
+from reciprocating.model import CompressorModel
+
+if __name__ == "__main__":
+
+    parameters = {  'bore diameter' : 0.780,
+                    'stroke' : 0.33,
+                    'connecting rod length' : 1.25,
+                    'rod diameter' : 0.135,
+                    'pressure ratio' : 1.90788804,
+                    'clearance (HE)' : 15.8,
+                    'clearance (CE)' : 18.39,
+                    'TDC crank angle 1' : 0,
+                    'rotational speed' : 360,
+                    'capacity' : 100,
+                    'acting label' : 0,
+                    'pressure at suction' : 19.65,
+                    'temperature at suction' : 45,
+                    'pressure unit' : "bar",
+                    'temperature unit' : "°C",
+                    'isentropic exponent' : 1.400,
+                    'molar mass' : 2.01568 }
+
+    compressor = CompressorModel(parameters)
+    compressor.set_fluid_properties_and_update_state(   parameters['isentropic exponent'],
+                                                        parameters['molar mass']   )
+
+    compressor.number_of_cylinders = 1
+    compressor.number_points = 2000
+
+    compressor.plot_PV_diagram_both_ends()
+
+    # rho_suc = cylinder.rho_suc
+    # compressor.plot_rod_pressure_load_frequency(6)
+    # compressor.plot_PV_diagram_head_end()
+
+    # mass_in = compressor.get_in_mass_flow()
+    # mass_out = compressor.get_out_mass_flow()
+    # total_mass = compressor.total_mass_flow()
+    # print(mass_in, mass_out, 200*(mass_in-mass_out)/(mass_in+mass_out))
+    # print(total_mass)
+
+    # cap = cap/100
+    # res_cap = compressor.process_capacity(capacity=cap)
+    # print(res_cap)
+
+    # mass_flow_full_capacity = -np.mean(compressor.process_sum_of_volumetric_flow_rate('in_flow', capacity=1))*rho_suc
+    # partial_flow = -np.mean(compressor.process_sum_of_volumetric_flow_rate('in_flow', capacity=res_cap))*rho_suc
+    # print((partial_flow/mass_flow_full_capacity)*100)
